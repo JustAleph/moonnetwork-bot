2f374b92425b91c9e0b79d148c1f5511f068c05e
JustAleph <44878908+JustAleph@users.noreply.github.com>
2019-01-24 18:03:53 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,17 +7,17 @@ import time
 
 import os
 
-client = commands.Bot(command_prefix= "pon!")
+client = commands.Bot(command_prefix= "!")
 client.remove_command('help')
 
-chat_filter = ["PON!KLASSEN", "PON!HELP",]
+chat_filter = ["!ip", "!ip",]
 bypass_list = []
 
 @client.event
 async def on_ready():
-    await client.change_presence(game=discord.Game(name="Nu actief in " + str(len(client.servers)) + " servers."))
+    await client.change_presence(game=discord.Game(name="Nu actief in " + str(len(client.servers)) + " server(s)."))
     await asyncio.sleep(20)
-    await client.change_presence(game=discord.Game(name="pon!help"))
+    await client.change_presence(game=discord.Game(name="!help"))
 
     print('Logged in as')
     print(client.user.name)
@@ -26,23 +26,23 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-        if message.content.startswith('pon!help'):
+        if message.content.startswith('!help'):
             embed=discord.Embed(title="Dit zijn mijn commando's", url="", color=0x3498db)
-            embed.set_author(name="Pontes bot", icon_url="https://cdn.discordapp.com/avatars/231703783988527104/7012bfe50b480374b601dae2576a521c.png?size=128")
-            embed.add_field(name="pon!klassen", value="om een lijst te krijgen van de klassen waar ik het rooster van kan laten zien", inline=False)
-            embed.add_field(name="pon![je klas]", value="om het rooster van je klas te krijgen (als ik het heb).", inline=False)
-            embed.add_field(name="pon!help", value="toon dit bericht!", inline=False)
+            embed.set_author(name="MoonNetwork", icon_url="https://cdn.discordapp.com/icons/536582250721181737/a0595aceb96d5883aa08b338c787f8b0.png")
+            embed.add_field(name="test", value="test", inline=False)
+            embed.add_field(name="test", value="test", inline=False)
+            embed.add_field(name="test", value="test", inline=False)
             embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/393852367751086090.gif?v=1")
-            embed.set_footer(text="Pontes bot © 2018 | gemaakt door @JustAleph0001",icon_url= "https://cdn.discordapp.com/avatars/231703783988527104/7012bfe50b480374b601dae2576a521c.png?size=128")
+            embed.set_footer(text="MoonNetwork bot © 2018 | gemaakt door @JustAleph0001",icon_url= "https://cdn.discordapp.com/avatars/231703783988527104/7012bfe50b480374b601dae2576a521c.png?size=128")
             await client.send_message(message.author, embed=embed)
             await client.delete_message(message)
             sent = await client.send_message(message.channel, "Ik heb je een DM gestuurd!")
             await asyncio.sleep(6)
             await client.delete_message(sent)
  
-        elif message.content.startswith('pon!klassen'):
-            embed=discord.Embed(title="dit is de lijst met klassen waar ik het rooster van heb!", url="", color=0x3498db)
-            embed.set_author(name="Pontes bot", icon_url="https://cdn.discordapp.com/avatars/231703783988527104/7012bfe50b480374b601dae2576a521c.png?size=128")
+        elif message.content.startswith('!ip'):
+            embed=discord.Embed(title="dit is het IP van de server!", url="", color=0x3498db)
+            embed.set_author(name="MoonNetwork", icon_url="https://cdn.discordapp.com/icons/536582250721181737/a0595aceb96d5883aa08b338c787f8b0.png")
             embed.add_field(name="H3RB", value="type 'pon!H3RB' voor het rooster van H3RB", inline=False)
             embed.add_field(name="pon!klassen", value="krijg een lijst van alle klassen", inline=False)
             embed.add_field(name="pon!help", value="krijg meer informatie over deze bot", inline=False)
